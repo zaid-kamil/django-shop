7cce537078559804200f2533c68ada765c54b31a
zaidkamil <xaidmetamorphos@gmail.com>
2023-12-02 18:36:23 +0530

    registration ui

--- shop/models.py
+++ shop/models.py
@@ -12,7 +12,6 @@ class Category(models.Model):
     def __str__(self):
         return self.name
     
-    
 class Product(models.Model):
     name = models.CharField(max_length=255)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
