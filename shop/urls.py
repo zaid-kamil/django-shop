8c014a61fe34d4465a07023b7639481e8e6297ad
zaidkamil <xaidmetamorphos@gmail.com>
2023-12-02 19:42:36 +0530

    product list and details

--- shop/urls.py
+++ shop/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('products/all/', views.all_products, name='listing'),
+    path('products/<slug:slug>/', views.product_detail, name='detail'),
 ]
--- shop/views.py
+++ shop/views.py
@@ -8,3 +8,8 @@ def all_products(request):
     products = Product.objects.all().filter(available=True)
     ctx  = {'products': products}
     return render(request, 'shop/listing.html', ctx)
+
+def product_detail(request, slug):
+    product = Product.objects.get(slug=slug)
+    ctx = {'product': product}
+    return render(request, 'shop/detail.html', ctx)
