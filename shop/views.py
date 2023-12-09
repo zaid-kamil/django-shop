1c5bc9557c53c7252c625191da2a336cb217cf75
zaidkamil <xaidmetamorphos@gmail.com>
2023-12-09 21:14:14 +0530

    minor thinggs

--- shop/views.py
+++ shop/views.py
@@ -1,12 +1,24 @@
 from django.shortcuts import render
 from .models import Product, Category
+# pagination
+from django.core.paginator import Paginator, EmptyPage, InvalidPage
+from django.contrib import messages
 # Create your views here.
 def index(request):
     return render(request, 'shop/listing.html')
 
 def all_products(request):
-    products = Product.objects.all().filter(available=True)
-    ctx  = {'products': products}
+    products_all = Product.objects.all().filter(available=True)
+    paginator = Paginator(products_all, 8)
+    try:
+        page = int(request.GET.get('page', '1'))
+    except:
+        page = 1
+    try:
+        products = paginator.page(page)
+    except(EmptyPage, InvalidPage):
+        products = paginator.page(paginator.num_pages)
+    ctx = {'products': products}
     return render(request, 'shop/listing.html', ctx)
 
 def product_detail(request, slug):
